976d53720925d035160ec8985dbf4814166d3e32
gunio2322 <gunio2322@gmail.com>
2024-05-30 10:21:31 +0200

    initial

---
+++ src/pages/home/index.js
@@ -0,0 +1,98 @@
+import React from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  // dataabout,
+  // // meta,
+  // worktimeline,
+  // skills,
+  services,
+} from "../../content_option";
+
+import Typewriter from "typewriter-effect";
+import { introdata, meta } from "../../content_option";
+import { Link } from "react-router-dom";
+import profile5 from "../../assets/images/profile5.webp";
+export const Home = () => {
+  return (
+    <HelmetProvider>
+      <section id="home" className="home">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title> {meta.title}</title>
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <div className="intro_sec d-block d-lg-flex align-items-center ">
+          <div
+            className="h_bg-image order-1 order-lg-2 h-100 "
+            // style={{ backgroundImage: `url(${introdata.your_img_url})` }}
+            style={{ backgroundImage: `url(${profile5})` }}
+          ></div>
+          <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
+            <div className="align-self-center ">
+              <div className="intro mx-auto">
+                <h2 className="mb-1x">{introdata.title}</h2>
+                <h1 className="fluidz-48 mb-1x">
+                  <Typewriter
+                    options={{
+                      strings: [
+                        introdata.animated.first,
+                        introdata.animated.second,
+                        introdata.animated.third,
+                      ],
+                      autoStart: true,
+                      loop: true,
+                      deleteSpeed: 10,
+                    }}
+                  />
+                </h1>
+                <p className="mb-1x">{introdata.description}</p>
+                <div className="intro_btn-action pb-5">
+                  {/* <Link to="/portfolio" className="text_2">
+                    <div id="button_p" className="ac_btn btn ">
+                      My Portfolio
+                      <div className="ring one"></div>
+                      <div className="ring two"></div>
+                      <div className="ring three"></div>
+                    </div>
+                  </Link> */}
+                  <Link to="/contact">
+                    <div id="button_h" className="ac_btn btn">
+                      Contact Me
+                      <div className="ring one"></div>
+                      <div className="ring two"></div>
+                      <div className="ring three"></div>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <Row className="sec_sp">
+            <Col lang="5">
+              <h3 className="color_sec py-4">services</h3>
+            </Col>
+            <Col lg="7">
+              {services.map((data, i) => {
+                return (
+                  <div className="service_ py-4" key={i}>
+                    <h5 className="service__title">{data.title}</h5>
+                    <p className="service_desc">{data.description}</p>
+                  </div>
+                );
+              })}
+            </Col>
+          </Row>
+        </div>
+      </section>
+    </HelmetProvider>
+
+
+  );
+};
